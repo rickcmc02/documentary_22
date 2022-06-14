7df6f53b14d8ab244b9c0f3038b934e3ff764165
양희영 <aa87565115@gmail.com>
2022-06-15 03:52:04 +0900

    add color, set thumb

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import "assets/css/index.css";
 import Gate from "layouts/Gate";
 import App from "layouts/App";
 import Error from "layouts/Error";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/layouts/Gate.js
+++ src/layouts/Gate.js
@@ -5,8 +5,43 @@ const Gate = () => {
   const navigate = useNavigate();
 
   return (
-    <Grid>
-      <div onClick={() => navigate("/main")}>썸네일</div>
+    <Grid
+      className="backBright0"
+      sx={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Grid
+        sx={{
+          border: "1px solid var(--bright2)",
+          width: "50vmin",
+          height: "50vmin",
+          borderRadius: "25vmin",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Grid
+          className="backColor1 textBright9"
+          sx={{
+            width: "70%",
+            height: "70%",
+            borderRadius: "70%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            cursor: "pointer",
+          }}
+          onClick={() => navigate("/main")}
+        >
+          <div>썸네일</div>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
