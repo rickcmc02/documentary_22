0395ef48155c1f9db5489e89192cf0065838b43d
양희영 <aa87565115@gmail.com>
2022-07-11 09:02:34 +0900

    (morning) add profile image on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,8 @@ import { Button, Drawer, Grid } from "@mui/material";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
+import ProfileImg from "assets/img/profile_img.jpeg";
+
 const Header = ({ height }) => {
   const [isHeaderOpen, setIsHeaderOpen] = useState(false);
 
@@ -22,16 +24,21 @@ const Header = ({ height }) => {
           height: `${isHeaderOpen ? height * 8 * 4 : height * 8}px`,
           transition: "height 0.5s",
           backgroundColor: "var(--bright3)",
+          boxShadow: "var(--bright3) 0px 0px 20px 0px",
         }}
       >
-        <Grid>헤더</Grid>
+        <Grid className="flex" sx={{ px: 2.5, py: 1 }}>
+          <img src={ProfileImg} className="profileImg" />
+          {isHeaderOpen ? (
+            <Grid sx={{ ml: 3 }}>
+              <p>양희영</p>
+            </Grid>
+          ) : null}
+        </Grid>
         <Grid className="fullHeight flex">
-          <Button
-            endIcon={
-              isHeaderOpen ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />
-            }
-            onClick={isHeaderOpen ? handleDrawerClose : handleDrawerOpen}
-          ></Button>
+          <Button onClick={isHeaderOpen ? handleDrawerClose : handleDrawerOpen}>
+            {isHeaderOpen ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </Button>
         </Grid>
       </Grid>
     </header>
