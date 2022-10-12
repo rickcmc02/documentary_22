0c4f89a11e12bf1b8714680ab4992a909d8fdc36
Retriever Young <aa87565115@gmail.com>
2022-10-12 13:53:49 +0900

    add tech stack source data

---
+++ src/views/SourceData/TechStack.js
@@ -0,0 +1,31 @@
+import ReactLogo from "assets/img/logo/react.png";
+import JavascriptLogo from "assets/img/logo/javascript.png";
+import CypressLogo from "assets/img/logo/cypress.png";
+import MaterialLogo from "assets/img/logo/materialui.png";
+
+export const eduSource = [
+  {
+    content: "React",
+    // detail: "GPA 3.56 / 4.5",
+    // period: "`20 졸업",
+    imgSrc: ReactLogo,
+  },
+  {
+    content: "Javascript",
+    // detail: "프로젝트 우수상",
+    // period: "`20 수료",
+    imgSrc: JavascriptLogo,
+  },
+  {
+    content: "Cypress",
+    // detail: "IH (Intermediate High)",
+    // period: "`22 취득",
+    imgSrc: CypressLogo,
+  },
+  {
+    content: "Material UI",
+    //   detail: "경제이해도 최우수등급 (상위 0.3%)",
+    //   period: "`21 취득",
+    imgSrc: MaterialLogo,
+  },
+];
