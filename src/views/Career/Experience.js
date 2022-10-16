6429c8a9ed21f32f945dbd8481ca2488d80b0e29
Retriever Young <aa87565115@gmail.com>
2022-10-16 23:43:41 +0900

    feat: update experience data

--- src/views/Career/Experience.js
+++ src/views/Career/Experience.js
@@ -1,16 +1,13 @@
-// import dslabLogo from "assets/img/logo_dslabglobal"
+import { expSource } from "../SourceData/Experience";
+import ShowSource from "components/Display/ShowSource";
 
 import { Grid } from "@mui/material";
 
 function Experience() {
   return (
-    <div>
-      <Grid>DSLABGLOBAL 프론트엔지니어 `21.05 ~ 현재</Grid>
-      <Grid>KICPA 공인회계사 수험 `17.02 ~ 20.02</Grid>
-      <Grid>현대글로비스 기획재정본부 인턴 `16.07 ~ `16.08</Grid>
-      <Grid>JTBC 보도국 정치부회의 인턴기자 `15.12 ~16.02</Grid>
-      <Grid>YCV 연세전산자원봉사회 `10.03 ~ 15.02</Grid>
-    </div>
+    <Grid>
+      <ShowSource sourceData={expSource} />
+    </Grid>
   );
 }
 export default Experience;
---
+++ src/views/SourceData/Experience.js
@@ -0,0 +1,38 @@
+import DslabLogo from "assets/img/logo/dslabglobal.jpeg";
+// import EncoreLogo from "assets/img/logo/encore.png";
+// import OpicLogo from "assets/img/logo/opic.png";
+// import MKTestLogo from "assets/img/logo/mktest.png";
+// import SamilLogo from "assets/img/logo/samil.png";
+
+export const expSource = [
+  {
+    content: "DSLABGLOBAL",
+    detail: "Front-end engineer",
+    period: "`20.05 ~",
+    imgSrc: DslabLogo,
+  },
+  //   {
+  //     content: "엔코아 Playdata 개발자 양성과정",
+  //     detail: "프로젝트 우수상",
+  //     period: "`20 수료",
+  //     imgSrc: EncoreLogo,
+  //   },
+  //   {
+  //     content: "OPIc 영어말하기테스트",
+  //     detail: "IH (Intermediate High)",
+  //     period: "`22 취득",
+  //     imgSrc: OpicLogo,
+  //   },
+  //   {
+  //     content: "국가공인 매경TEST",
+  //     detail: "경제이해도 최우수등급 (상위 0.3%)",
+  //     period: "`21 취득",
+  //     imgSrc: MKTestLogo,
+  //   },
+  //   {
+  //     content: "삼일회계법인 회계관리1급",
+  //     detail: "회계, 세법",
+  //     period: "`21 취득",
+  //     imgSrc: SamilLogo,
+  //   },
+];
