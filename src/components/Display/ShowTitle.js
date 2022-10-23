0097688fcc01d6f6d1288630d78aa85e76ef05ee
Retriever Young <aa87565115@gmail.com>
2022-10-24 00:37:15 +0900

    feat: add text color on title

--- src/components/Display/ShowTitle.js
+++ src/components/Display/ShowTitle.js
@@ -5,18 +5,25 @@ function ShowTitle({ sourceData }) {
     <Grid sx={{ py: 2, px: 1, mb: 2 }}>
       <Grid sx={{ mb: 2 }}>
         <Grid sx={{ mb: 0.5 }}>
-          <span className="size20 weight700">{sourceData.mainHead}</span>
+          <span className="size20 weight700 colorBright7">
+            {sourceData.mainHead}
+          </span>
         </Grid>
         <Grid>
-          <span className="size18 weight300">{sourceData.subHead}</span>
+          <span className="size18 weight300 colorBright7">
+            {sourceData.subHead}
+          </span>
         </Grid>
       </Grid>
       <Grid>
-        <span style={{ lineHeight: 1.7 }}>
+        <span className="colorBright7" style={{ lineHeight: 1.7 }}>
           {sourceData.mainDescription}
           <br />
         </span>
-        <span className="weight300 size15" style={{ lineHeight: 1.7 }}>
+        <span
+          className="weight300 size15 colorBright7"
+          style={{ lineHeight: 1.7 }}
+        >
           {sourceData.subDescription?.map((desc) => (
             <>
               {desc}
